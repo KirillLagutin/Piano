9e13ba4ad12c925f2757cfdf17051cb543dd6c5f
Kirill Lagutin <lagutin.k@mail.ru>
2022-07-10 01:19:44 +0300

    Add keyboard clicks

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-let keys = document.querySelectorAll('.key');
+document.addEventListener('keydown', playNote);
 
+let keys = document.querySelectorAll('.key');
 keys.forEach(key => {
     key.addEventListener('click', playNote);
 });
 
 function playNote(e) {
-    let key = e.target;
-    let note = document.getElementById(key.dataset.note);
-    key.classList.add('active');
-    note.currentTime = 0;
-    note.play();
-    note.addEventListener('ended', () => {
+    let key, audio;
+    
+    if(e.keyCode) {
+        key = document.querySelector(`[data-note="${e.keyCode}"]`);
+        audio = document.getElementById(e.keyCode);
+    }
+    else {
+        key = e.target;
+        audio = document.getElementById(key.dataset.note);
+    }
+
+    key.classList.add('active')
+
+    audio.currentTime = 0;
+    audio.play();
+    audio.addEventListener('ended', () => {
         key.classList.remove('active');
     });
 }
